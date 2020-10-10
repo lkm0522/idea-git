af9cb319008ae898a31854e2aef74cc6a463bc53
lkm0522 <1277347188@qq.com>
2020-10-10 17:12:24 +0800

    add hello

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * Create By 木木 On 2020.10.10 17:17  星期六
+ * Desc:
+ */
+public class Test
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hello world");
+    }
+}
